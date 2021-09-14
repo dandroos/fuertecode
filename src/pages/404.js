8a6e80eec1f70cd1bae124588774334da08bdbc0
Dave Androos <daveandrooses@gmail.com>
2021-09-14 11:35:10 +0100

    amended 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,17 @@
+import { Typography } from "@material-ui/core"
 import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Typography variant="h2" gutterBottom>
+      Hmmmm....
+    </Typography>
+    <Typography>That page doesn't seem to exist...spooky!</Typography>
+  </>
 )
 
 export default NotFoundPage
